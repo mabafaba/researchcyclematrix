60d506f65c5b5e58831c61445a50bb5f79371bfc
Martin <martin.barner@impact-initiatives.org>
2019-05-08 21:50:17 +0200

    cleanup

--- R/rcm_check_csv.R
+++ R/rcm_check_csv.R
@@ -7,15 +7,17 @@
 #' @param rcm use rcm_download(gdrive_links=F). can be ommited (then downloading from gdrive)
 #' @return writes csv files to current wd, and also opens them in excel directly!
 #' @export
-rcm_check_csv<-function(rcm=NULL){
+rcm_check_csv<-function(rcm=NULL, unit = "data"){
 
 if(is.null(rcm)){
   rcm<-rcm_download(include_archived = F,include_validated = F,gdrive_links = F)
 }
 
-
+rcm<-rcm[grepl(unit,rcm$unit),]
 inconsistencies<-rcm_check(rcm,check.archived = F,check.validated = F)
 inconsistencies<-inconsistencies[inconsistencies$issue!="data unit item with non-standardisable status",]
+inconsistencies <- inconsistencies %>% arrange(rcid)
+
 inconsistencies %>% split.data.frame(hq_focal_point(inconsistencies$rcid)) %>% mapply(.,names(.),FUN = function(inc,who){
   thisfile<-paste0("inconsistencies_",who,".csv")
   write.csv(inc,file = thisfile,row.names = F)
